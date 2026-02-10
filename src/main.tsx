e25d75e1dbd8013aead647cc02eea10f5749daa1
1vanSher <ivan.sher.05@mail.ru>
2026-02-11 00:06:55 +0300

    add global styles, mixins and variables styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
 import App from './app/App.tsx';
+import './shared/styles/global.scss';
 
 const rootElement = document.getElementById('root');
 
